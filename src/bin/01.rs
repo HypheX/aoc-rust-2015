dd4b1c3179a3c78e31eb5a85601dd63eeedf7453
Xelph <fierrosalinas.la@gmail.com>
2024-10-18 23:06:25 -0600

    Solve day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,10 +1,29 @@
 advent_of_code::solution!(1);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<i32> {
+    let mut accumulator = 0_i32;
+    for ch in input.chars() {
+        match ch {
+            '(' => accumulator += 1,
+            ')' => accumulator -= 1,
+            _ => {}
+        }
+    }
+    Some(accumulator)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut accumulator = 0_i32;
+    for (index, ch) in input.chars().enumerate() {
+        match ch {
+            '(' => accumulator += 1,
+            ')' => accumulator -= 1,
+            _ => {}
+        }
+        if accumulator < 0 {
+            return Some(index + 1);
+        }
+    }
     None
 }
 
